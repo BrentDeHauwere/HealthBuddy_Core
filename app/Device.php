2a8ca469f46dbdce4bb0034c9c3ba586fca2c303
Brent De Hauwere <brentdehauwere@gmail.com>
2016-03-08 22:32:39 +0100

    Models - Define PK

--- app/Device.php
+++ app/Device.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Device extends Model
 {
-    //
+    /**
+     * Eloquent will also assume that each table has a primary key column named id.
+     * You may define a $primaryKey property to override this convention.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'device_id';
 }
