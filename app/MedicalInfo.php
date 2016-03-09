f347f98206e9ff3977f57bd0c7ed5fce9a90f1c1
Brent De Hauwere <brentdehauwere@gmail.com>
2016-03-09 22:48:12 +0100

    Ophalen user met medical info

--- app/MedicalInfo.php
+++ app/MedicalInfo.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class MedicalInfo extends Model
 {
     /**
+    * The table associated with the model.
+    *
+    * @var string
+    */
+    protected $table = 'medicalInfos';
+
+    /**
      * Get the user that owns the medicalInfo.
      */
     public function user()
--- database/seeds/MedicalInfosTableSeeder.php
+++ database/seeds/MedicalInfosTableSeeder.php
@@ -12,10 +12,17 @@ class MedicalInfosTableSeeder extends Seeder
     public function run()
     {
         DB::table('medicalInfos')->insert([
-            'user_id' => 1,
+            'user_id' => 2,
             'length' => 182,
-            'weight' => 68.50,
+            'weight' => 63.50,
             'bloodType' => 'A+'
+        ]);
+
+        DB::table('medicalInfos')->insert([
+            'user_id' => 3,
+            'length' => 182,
+            'weight' => 78.3,
+            'bloodType' => 'B-'
         ]);
     }
 }
