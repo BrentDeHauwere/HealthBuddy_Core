82a674031091d58a851556e487c99b963ae8a212
eddii <eddii.ehb@gmail.com>
2016-03-10 09:02:44 +0100

    Added authentication for website

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -6,3 +6,9 @@ Route::get('/', function () {
 
 Route::get('user/{user}', 'UserController@showProfile');
 Route::get('patients/{user}', 'UserController@showPatients');
+
+Route::group(['middleware' => 'web'], function () {
+    Route::auth();
+
+    Route::get('/home', 'HomeController@index');
+});
