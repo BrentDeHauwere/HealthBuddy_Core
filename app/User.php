62f00016255d51e18d50056632fcfb5da85aee24
Brent De Hauwere <brentdehauwere@gmail.com>
2016-03-10 08:49:00 +0100

    Authenticatable - 2

--- app/User.php
+++ app/User.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model, Authenticatable
+class User extends Authenticatable
 {
     /**
      * Hide certain columns.
