7f5d146518df570e86ed2a9cac0f83c466a50451
Eli Boey <eli.boey@student.ehb.be>
2016-03-16 14:14:16 +0100

    Modal change
    Modal change

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -39,9 +39,9 @@
                   <td>{{ $user->email }}</td>
                   <td>{{ $user->role }}</td>
                   <td>
-                      <button type="button" class="btn btn-primary EditUser" data-toggle="modal" data-target="#EditModal">Edit</button>
-                      <button type="button" class="btn btn-primary ResetPass" data-toggle="modal" data-target="#ResetModal">Password</button>
-                      <button type="button" class="btn btn-primary LinkDev" data-toggle="modal" data-target="#LinkModal">Devices</button>
+                      <button type="button" class="btn btn-primary EditUser"  data-target="#EditModal">Edit</button>
+                      <button type="button" class="btn btn-primary ResetPass"  data-target="#ResetModal">Password</button>
+                      <button type="button" class="btn btn-primary LinkDev"  data-target="#LinkModal">Devices</button>
                       <input type="hidden" value="{{ $user->id }}" name="ID"/>
                   </td>
                 </tr>
@@ -50,82 +50,7 @@
         </div>
     </div>
 </div>
-<div id="AddModal" class="modal fade" role="dialog">
-  <div class="modal-dialog">
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Add a new user</h4>
-      </div>
-      <div class="modal-body">
-
-      </div>
-      <div class="modal-footer">
-        <input type="submit" class="btn btn-primary" name="submit" value="Submit"/>
-        </form>
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
-<div id="EditModal" class="modal fade" role="dialog">
-  <div class="modal-dialog">
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Edit a user</h4>
-      </div>
-      <div class="modal-body">
-
-      </div>
-      <div class="modal-footer">
-        <input type="submit" class="btn btn-primary" name="submit" value="Submit"/>
-        </form>
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
-<div id="ResetModal" class="modal fade" role="dialog">
-  <div class="modal-dialog">
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Reset password of a user</h4>
-      </div>
-      <div class="modal-body">
-
-      </div>
-      <div class="modal-footer">
-        <input type="submit" class="btn btn-primary" name="submit" value="Submit"/>
-        </form>
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
-<div id="LinkModal" class="modal fade" role="dialog">
-  <div class="modal-dialog">
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Link a user to a device</h4>
-      </div>
-      <div class="modal-body">
-
-      </div>
-      <div class="modal-footer">
-        <input type="submit" class="btn btn-primary" name="submit" value="Submit"/>
-        </form>
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
+<div id="modal" class="modal fade" role="dialog"></div>
 @endsection
 @section('footer')
 <script type="text/javascript">
@@ -168,11 +93,13 @@
       cache: false,
       success: function(data){
           console.log(button);
-         $($(button).data("target")).find(".modal-body").html(data);
+         $("#modal").html(data);
+         $('#modal').modal('show');
       },
       error: function(error){
         console.log("error");
-        $($(button).data("target")).find(".modal-body").html("Something went wrong");
+        $("#modal").html("Something went wrong");
+        $('#modal').modal('show');
       }
     });
 
--- resources/views/resetmodal.blade.php
+++ resources/views/resetmodal.blade.php
@@ -1,14 +1,30 @@
-<form class="form-horizontal">
-  <input type="hidden" name="id" value="{{ $user->id }}">
-  <div class="form-group">
-    <label for="password" class="col-sm-2 control-label">Password</label>
-    <div class="col-sm-10">
-      <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+<div class="modal-dialog">
+  <!-- Modal content-->
+  <div class="modal-content">
+    <div class="modal-header">
+      <button type="button" class="close" data-dismiss="modal">&times;</button>
+      <h4 class="modal-title">Change password of a user</h4>
+    </div>
+    <div class="modal-body">
+      <form class="form-horizontal">
+        <input type="hidden" name="id" value="{{ $user->id }}">
+        <div class="form-group">
+          <label for="password" class="col-sm-2 control-label">Password</label>
+          <div class="col-sm-10">
+            <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="confirm" class="col-sm-2 control-label">Confirm</label>
+          <div class="col-sm-10">
+            <input type="password" class="form-control" id="confirm" name="confirm" placeholder="Confirm">
+          </div>
+        </div>
+    </div>
+    <div class="modal-footer">
+      <input type="submit" class="btn btn-primary" name="submit" value="Submit"/>
+      </form>
+      <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
     </div>
   </div>
-  <div class="form-group">
-    <label for="confirm" class="col-sm-2 control-label">Confirm</label>
-    <div class="col-sm-10">
-      <input type="password" class="form-control" id="confirm" name="confirm" placeholder="Confirm">
-    </div>
-  </div>
+</div>
