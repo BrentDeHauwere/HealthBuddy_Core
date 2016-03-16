0071e662c0157514e34bd28b41d2f2471e07b480
Eli Boey <eli.boey@student.ehb.be>
2016-03-16 14:18:31 +0100

    Master issue gone
    gone

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -31,18 +31,18 @@ Route::group(['middleware' => 'web'], function () {
     Route::auth();
     Route::get('/home', 'HomeController@index');
 
-<<<<<<< HEAD
+
     Route::post('/addmodal','ModalController@add');
     Route::post('/editmodal','ModalController@edit');
     Route::post('/resetmodal','ModalController@reset');
     Route::post('/linkmodal','ModalController@link');
-=======
+
     Route::get('user/{user}', 'UserController@showProfile');
     Route::get('patients/{user}', 'UserController@showPatients');
     Route::get('devices/{user}', 'UserController@showDevices');
     Route::get('weights/{user}', 'UserController@showWeights');
     Route::get('schedule/{user}', 'UserController@showSchedule');
->>>>>>> master
+
 });
 
 
