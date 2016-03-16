d7a629e69ae183e379e4e89665be1662b26cf74a
Eli Boey <eli.boey@student.ehb.be>
2016-03-16 15:45:29 +0100

    if's for add user
    add user if’s

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -63,7 +63,31 @@ class UserController extends Controller
           $user = \App\User::where('email','=',$request->input('confirm'))->first();
           if(!($user)){
             if(preg_match('/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/', $request->input('password')) === 1){
-              return redirect('/home')->with('success','Success');
+              if(preg_match('/^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})/', $request->input('email')) === 1){
+                if($request->input('gender') == 'M' || $request->input('gender') == 'V'){
+                  if($request->input('role') == 'Zorgwinkel' || $request->input('role') == 'Zorgmantel' || $request->input('role') == 'Zorgbehoevende'){
+                    $firstname = $request->('firstname');
+                    $lastname = $request->('lastname');
+                    $password = $request->('password');
+                    $date = $request->('date');
+                    $email = $request->('email');
+                    $gender = $request->('gender');
+                    $role = $request->('role');
+                    //\App\User::;
+                    return redirect('/home')->with('success','Success');
+                  }
+                  else{
+                    return redirect('/home')->with('response','Role cannot be this value');
+                  }
+                }
+                else{
+                  return redirect('/home')->with('response','Gender can only be M or V');
+                }
+
+              }
+              else{
+                return redirect('/home')->with('response','This is not a valid email');
+              }
             }
             else{
               return redirect('/home')->with('response','Password must have atleast 8 characters and both Alphabetic and number');
