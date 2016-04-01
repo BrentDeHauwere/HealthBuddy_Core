51485397f93381cd6c7cbcb10dcb234a887d5773
Eli Boey <eli.boey@student.ehb.be>
2016-04-01 15:24:33 +0200

    quick fix succesmodal
    quick fix

--- resources/views/modals/successmodal.blade.php
+++ resources/views/modals/successmodal.blade.php
@@ -6,7 +6,7 @@
 </style>
 <div class="col-md-6 col-md-offset-3 top">
   <div class="alert alert-success fade in center-block">
-    <strong>Success!</strong> {{ $error }}
+    <strong>Success!</strong> {{ $success }}
     <button type="button" class="close" data-dismiss="modal">&times;</button>
   </div>
 </div>
