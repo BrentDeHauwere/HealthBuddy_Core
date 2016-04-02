d30c8a079533b86cc016ffd1adf1eda2fa2454df
eddii <eddii.ehb@gmail.com>
2016-04-02 17:09:19 +0200

    nederlandse validation messages

--- app/Http/Requests/Request.php
+++ app/Http/Requests/Request.php
@@ -35,21 +35,22 @@ abstract class Request extends FormRequest
     public function messages()
     {
         return [
-            'required'  => 'Het :attribute veld is verplicht in te vullen.',
-            'in'        => 'Het :attribute veld moet een van de volgende waarden bevatten: :values',
-            'size'      => 'Het :attribute veld moet :size lang zijn.',
-            'between'   => 'Het :attribute veld moet tussen :min - :max zijn.',
-            'min'       => 'Het :attribute veld moet minstens :min tekens lang zijn.',
-            'max'       => 'Het :attribute veld mag maximaal :max tekens lang zijn.',
-            'unique'    => 'Het :attribute veld moet uniek zijn.',
-            'alpha_num' => 'Het :attribute veld moet alfanumeriek zijn.',
-            'numberic'  => 'Het :attribute veld moet numeriek zijn.',
-            'email'     => 'Het :attribute veld moet een geldig emailadres zijn.',
-            'image'     => 'Het :attribute veld moet een foto zijn, een jpeg,png, bmp, gif of svg',
-            'same'      => 'Het :attribute veld moet gelijk zijn aan :other.',
-            'before'    => 'De :attribute moet voor :date zijn',
-            'after'     => 'De :attribute moet na :date zijn',
-            'alpha'     => 'Het :attribute veld mag enkel uit letters bestaan',
+            'required'      => 'Het :attribute veld is verplicht in te vullen.',
+            'in'            => 'Het :attribute veld moet een van de volgende waarden bevatten: :values',
+            'size'          => 'Het :attribute veld moet :size lang zijn.',
+            'between'       => 'Het :attribute veld moet tussen :min - :max zijn.',
+            'min'           => 'Het :attribute veld moet minstens :min tekens lang zijn.',
+            'max'           => 'Het :attribute veld mag maximaal :max tekens lang zijn.',
+            'unique'        => 'Het :attribute veld moet uniek zijn.',
+            'alpha_num'     => 'Het :attribute veld moet alfanumeriek zijn.',
+            'numberic'      => 'Het :attribute veld moet numeriek zijn.',
+            'email'         => 'Het :attribute veld moet een geldig emailadres zijn.',
+            'image'         => 'Het :attribute veld moet een foto zijn, een jpeg,png, bmp, gif of svg',
+            'same'          => 'Het :attribute veld moet gelijk zijn aan :other.',
+            'before'        => 'De :attribute moet voor :date zijn',
+            'after'         => 'De :attribute moet na :date zijn',
+            'alpha'         => 'Het :attribute veld mag enkel uit letters bestaan',
+            'date_format'   => 'Het :attribute veld moet een geldige tijd bevatten',
         ];
     }
 }
--- app/Http/Requests/UpdateMedicalInfoApiRequest.php
+++ app/Http/Requests/UpdateMedicalInfoApiRequest.php
@@ -47,4 +47,37 @@ class UpdateMedicalInfoApiRequest extends Request
         'allergies'         => 'max:255',
         ];
     }
+
+    /**
+    * Get the error messages for the defined validation rules.
+    * Maybe not the cleanest solution, but certainly does the job.
+    * 
+    * @author eddi
+    * @return array
+    */
+      public function messages()
+      {
+
+        $fields = array('length' => 'lengte', 'weight' => 'gewicht', 'bloodType' => 'bloedtype', 'medicalCondition'=>'medische aandoening', 'allergies'=>'allergiën');
+        $msgs = array();
+
+        foreach ($fields as $f => $k) {
+          $msgs[$f.'.required']  = 'Het ' . $k .' veld is verplicht in te vullen.';
+          $msgs[$f.'.size']      = 'Het ' . $k . ' veld moet :size lang zijn.';
+          $msgs[$f.'.between']   = 'Het ' . $k . ' veld moet tussen :min - :max zijn.';
+          $msgs[$f.'.min']       = 'Het ' . $k . ' veld moet minstens :min tekens lang zijn.';
+          $msgs[$f.'.max']       = 'Het ' . $k . ' veld mag maximaal :max tekens lang zijn.';
+          $msgs[$f.'.unique']    = 'Het ' . $k . ' veld moet uniek zijn.';
+          $msgs[$f.'.alpha_num'] = 'Het ' . $k . ' veld moet alfanumeriek zijn.';
+          $msgs[$f.'.numberic']  = 'Het ' . $k . ' veld moet numeriek zijn.';
+          $msgs[$f.'.email']     = 'Het ' . $k . ' veld moet een geldig emailadres zijn.';
+          $msgs[$f.'.image']     = 'Het ' . $k . ' veld moet een foto zijn, een jpeg,png, bmp, gif of svg';
+          $msgs[$f.'.same']      = 'Het ' . $k . ' veld moet gelijk zijn aan :other.';
+          $msgs[$f.'.before']    = 'De ' . $k . ' moet voor :date zijn';
+          $msgs[$f.'.after']     = 'De ' . $k . ' moet na :date zijn';
+          $msgs[$f.'.alpha']     = 'Het ' . $k . ' veld mag enkel uit letters bestaan';
+        }
+
+        return $msgs;
+      }
 }
