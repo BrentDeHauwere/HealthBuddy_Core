8bfac111cfa71796b59dbb26b9a1a3f6e4ed6577
eddii <eddii.ehb@gmail.com>
2016-04-06 09:22:25 +0200

    added more translations

---
+++ resources/lang/nl/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Wachtwoorden moeten ten minste zes karakters lang zijn en overeenkomen met het bevestigingswachtwoord',
+    'reset' => 'Uw wachtwoord is gereset!',
+    'sent' => 'We hebben u een wachtwoord reset mail gestuurd!',
+    'token' => 'Deze wachtwoord reset token is ongeldig',
+    'user' => "We kunnen geen gebruiker vinden met dit e-mailadres",
+
+];
