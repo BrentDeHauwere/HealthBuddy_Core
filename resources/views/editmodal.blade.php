272dc7293a474ae4560ea42a5dce2eb69ede570d
De Hauwere Brent <brent.de.hauwere@student.ehb.be>
2016-04-27 13:17:12 +0200

    Edit a user -> Edit Brent De Hauwere

--- resources/views/editmodal.blade.php
+++ resources/views/editmodal.blade.php
@@ -3,7 +3,7 @@
   <div class="modal-content">
     <div class="modal-header">
       <button type="button" class="close" data-dismiss="modal">&times;</button>
-      <h4 class="modal-title">Edit a user</h4>
+      <h4 class="modal-title">Edit {{ $user->firstName . ' ' . $user->lastName }}</h4>
     </div>
     <div class="modal-body">
       <form class="form-horizontal">
